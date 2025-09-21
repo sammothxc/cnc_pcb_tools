9f3a6b1ca9a5e0d6dd4a414229081c7f5eee0d18
Sam Warr <sam.warr.tx@gmail.com>
2025-09-20 20:09:25 -0600

    Update check_pcb_common.py

--- check_pcb_common.py
+++ check_pcb_common.py
@@ -1,6 +1,6 @@
 """Common checks that would go in every file."""
 
-def check_common(check, maxx=100.0, miny=-70.0):
+def check_common(check, maxx=100.0, miny=-20):
   """Common checks that would otherwise have to be repeated in every file."""
   check.dump_properties()
   check.assert_spinning()
@@ -8,7 +8,7 @@ def check_common(check, maxx=100.0, miny=-70.0):
   check.assert_gt('min_x', check.min_x(), -2.0)
   check.assert_lt('max_x', check.max_x(), maxx)
   check.assert_gt('min_y', check.min_y(), miny)
-  check.assert_lt('max_y', check.max_y(), 2.0)
+  check.assert_lt('max_y', check.max_y(), 70.1)
 
   # maximum allowed z
   check.assert_lt('max_z', check.max_z(), 15.1)
